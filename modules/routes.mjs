d23cd66b84de47114cfe3a5d4ffa5e8766253f6e
Pigstin <afcb1379@gmail.com>
2026-09-20 22:02:41 -0400

    fixed removal

--- modules/routes.mjs
+++ modules/routes.mjs
@@ -19,17 +19,8 @@ export function add(options) {
         // added to server here
         const result = await options.col.insertOne( req.body )
 
-        // go through data and amend to add order field
-        let data = await options.col.find({}).toArray()
-        let appdata = new AppData(data)
-        appdata.orderEntries()
-        appdata.entries.forEach( async (element) => {
-            console.log(element)
-            await options.col.updateOne(
-                { _id: new ObjectId( element._id ) },
-                { $set:{ order:element.order } }
-            )
-        });
+        // update order
+        updateOrder(options);
 
         res.json( result )
     }
@@ -38,20 +29,49 @@ export function add(options) {
 export function remove(options) {
     // assumes req.body takes form { _id:5d91fb30f3f81b282d7be0dd } etc.
     return async (req,res) => {
-        const result = await options.col.deleteOne({ 
-            _id: new ObjectId( req.body.id ) 
-        })
-        res.json( result )
+        console.log(req.body.index)
+        const findInter = await options.col.find()
+        let delres; 
+        // yes this solution hurts my soul. no I could not find a better one.
+        for await(let doc of findInter) {
+            if(doc.order == req.body.index) {
+                delres = await options.col.deleteOne(doc)
+            }
+        }
+        updateOrder()
+        res.json( delres )
     }
 }
 
 export function update(options) {
     return async (req,res) => {
-        console.log(req.body)
-        const result = await options.col.updateOne(
-            { _id: new ObjectId( req.body.id ) },
-            { $set:{ name:req.body.name } }
-        )
+        console.log("old object:")
+        console.log(req.body.oldObj)
+        console.log("new object:")
+        console.log(req.body.newObj)
+        
+        
+        // const result = await options.col.updateOne(
+        //     { _id: new ObjectId( req.body.id ) },
+        //     { $set:{ name:req.body.name } }
+        // )
+
+        // up until here order is not changed for either object
+        updateOrder()
         res.json( result )
     }
 }
+
+async function updateOrder(options) {
+    // go through data and amend to add order field
+    let data = await options.col.find({}).toArray()
+    let appdata = new AppData(data)
+    appdata.orderEntries()
+    appdata.entries.forEach( async (element) => {
+        console.log(element)
+        await options.col.updateOne(
+            { _id: new ObjectId( element._id ) },
+            { $set:{ order:element.order } }
+        )
+    });
+} 
